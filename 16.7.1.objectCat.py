18243984ff1b4506aefbccffc83ec55feb51b9d9
viktoriastash <89913068+viktoriastash@users.noreply.github.com>
2021-11-08 14:58:46 +0200

    Create 16.7.1.objectCat.py

---
+++ 16.7.1.objectCat.py
@@ -0,0 +1,12 @@
+from Class_Cat import Cat
+
+cat_Baron = Cat("Барон", "Мальчик", 2)
+cat_Sam = Cat("Сэм", "Мальчик", 2)
+
+print("cat_Baron.name ", cat_Baron.name)
+print("cat_Baron.gender ", cat_Baron.gender)
+print("cat_Baron.age ", str(cat_Baron.age))
+
+print("cat_Sam.name ", cat_Sam.name)
+print("cat_Sam.gender ", cat_Sam.gender)
+print("cat_Sam.age ", str(cat_Sam.age))
