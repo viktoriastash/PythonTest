d36b7afbe4fbf2c0ee50a33a091f4b0e455d8b04
viktoriastash <89913068+viktoriastash@users.noreply.github.com>
2021-11-20 16:36:47 +0200

    Create ConfigToken.py

---
+++ ConfigToken.py
@@ -0,0 +1,10 @@
+TOKEN = '2118051062:AAFApGz962BIioFQ2MYmJI8ChwMcnVxmIKs'
+
+keys = {
+    'рубль': 'RUR',
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'юань': 'CNY',
+    'японская_иена': 'JPY',
+    'фунт_стерлингов': 'GBP'
+  }
